78b89ef3f72f1d516319b1cd099ff71066ad423f
lauren4242 <89677512+lauren4242@users.noreply.github.com>
2026-02-04 10:36:58 -0700

    Refactor pingsweep.sh to use functions and options
    Refactored script to include a ping sweep function and added command-line options for help and execution.

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 # Ping sweep the Lab
 
-base="onyxnode"
+ping_sweep(){
+	base="onyxnode"
+	spin='|/-\\'
+	for q in {1..200}
+	do
+		host="onyxnode$q"
+		i=$((q % 4))
+		printf "\r%c %d out of 200" "${spin:$i:1}" "$q"
+		if ping -c 1 -W 1 "$host" >> ping.log 2>&1; then
+			echo -e "\r$host true"
+		else
+			echo -e "\r$host false"
+		fi
+	done
+}
+# have a help menu
+help(){
+	echo "Usage: pingsweep.sh [-p] [-h]"
+	echo "Options:"
+	echo "  -p    Run ping sweep"
+	echo "  -h    Show this help menu"
+	exit 1
+}
 
-for q in {1..200}
-do
-	curr="$base$q"
-	ping -c 1 $curr >> ping.log
+# add a flag -p to run it
+while getopts "ph" opt; do
+	case $opt in
+		p)
+			ping_sweep
+			;;
+		h)
+			help
+			;;
+		*)
+			help
+			;;
+	esac
 done
 
